f8d688b70e375fb987a134e8358b2f171dbb8c4d
wenlong lyu <lvwenlong_lambda@qq.com>
2019-04-23 15:41:36 +0800

    Use new encoder

--- VAE.py
+++ VAE.py
@@ -1,3 +1,4 @@
+import sys
 import torch
 import numpy as np
 import pyro
@@ -11,6 +12,55 @@ from   pyro.infer import SVI, Trace_ELBO
 from   pyro.optim import Adam
 from   tqdm import tqdm
 
+class Encoder(nn.Module):
+    def __init__(self, n_channel, img_size, z_dim):
+        super(Encoder, self).__init__()
+        self.img_size  = img_size
+        self.n_channel = n_channel
+        self.z_dim     = z_dim
+
+
+        # # Directly use resnet18 as feature extractor
+        # self.model    = vision.models.resnet18()
+        # self.model.fc = nn.Linear(512, self.z_dim * 2)
+        
+
+        ndf         = 64 # number of filters
+        kernel_size = 4
+        stride      = 2
+        padding     = 1
+        self.ndf    = ndf
+
+        self.conv1  = nn.Conv2d(self.n_channel, ndf, kernel_size = kernel_size, stride = stride, padding= padding, bias = False)
+
+        self.conv2  = nn.Conv2d(1 * ndf, 2 * ndf, kernel_size = kernel_size, stride = stride, padding= padding, bias = False)
+        self.bn2    = nn.BatchNorm2d(2 * ndf)
+
+        self.conv3  = nn.Conv2d(2 * ndf, 4 * ndf, kernel_size = kernel_size, stride = stride, padding= padding, bias = False)
+        self.bn3    = nn.BatchNorm2d(4 * ndf)
+
+        self.conv4  = nn.Conv2d(4 * ndf, 8 * ndf, kernel_size = kernel_size, stride = stride, padding= padding, bias = False)
+        self.bn4    = nn.BatchNorm2d(8 * ndf)
+
+        self.final_img_size = self.img_size // 16
+        self.fc             = nn.Linear(8 * ndf * self.final_img_size**2, 2 * self.z_dim)
+
+    def forward(self, x):
+        h1 = F.leaky_relu(self.conv1(x),            negative_slope=0.2)
+        h2 = F.leaky_relu(self.bn2(self.conv2(h1)), negative_slope=0.2)
+        h3 = F.leaky_relu(self.bn3(self.conv3(h2)), negative_slope=0.2)
+        h4 = F.leaky_relu(self.bn4(self.conv4(h3)), negative_slope=0.2)
+
+        zfeatures = self.fc(h4.view(-1, 8 * self.ndf * self.final_img_size**2))
+        z_loc     = zfeatures[:, :self.z_dim]
+        z_scale   = zfeatures[:, self.z_dim:]
+
+        # features = self.model(x)
+        # z_loc    = features[:, :self.z_dim].squeeze()
+        # z_scale  = features[:, self.z_dim:].squeeze()
+
+        return z_loc, F.softplus(z_scale) + 1e-6
+
 class Decoder(nn.Module):
     def __init__(self, n_channel, img_size, z_dim):
         super(Decoder, self).__init__()
@@ -19,28 +69,33 @@ class Decoder(nn.Module):
         self.n_channel = n_channel
         self.z_dim     = z_dim
 
-        ndf         = 64 # number of filters
+        ndf         = 32 # number of filters
         kernel_size = 4
         stride      = 2
         padding     = 1
+        self.ndf    = ndf
 
-        self.deconv1 = nn.ConvTranspose2d(self.z_dim, 8 * ndf, kernel_size = int(self.img_size / 64) * kernel_size, bias = False) #XXX
-        self.bn1     = nn.BatchNorm2d(8 * ndf) # 1024 * 8 * 8
+        self.init_size = self.img_size // 16
+
+        # self.deconv1 = nn.ConvTranspose2d(self.z_dim, 8 * ndf, kernel_size = int(self.img_size / 64) * kernel_size, bias = False) #XXX
+        # self.bn1       = nn.BatchNorm2d(8 * ndf)  
+        self.linear1   = nn.Linear(self.z_dim, 8 * ndf * self.init_size * self.init_size)
+        self.bn1       = nn.BatchNorm2d(8 * ndf)  
 
         self.deconv2 = nn.ConvTranspose2d(8 * ndf, 4 * ndf, kernel_size = kernel_size, stride = stride, padding = padding, bias = False)
-        self.bn2     = nn.BatchNorm2d(4 * ndf) # 512 * 16 * 16
+        self.bn2     = nn.BatchNorm2d(4 * ndf)  
 
         self.deconv3 = nn.ConvTranspose2d(4 * ndf, 2 * ndf, kernel_size = kernel_size, stride = stride, padding = padding, bias = False)
-        self.bn3     = nn.BatchNorm2d(2 * ndf) # 256 * 32 * 32
+        self.bn3     = nn.BatchNorm2d(2 * ndf)  
 
         self.deconv4 = nn.ConvTranspose2d(2 * ndf, ndf, kernel_size = kernel_size, stride = stride, padding = padding, bias = False)
-        self.bn4     = nn.BatchNorm2d(ndf) # 128 * 64 * 64
+        self.bn4     = nn.BatchNorm2d(ndf)      
 
-        self.deconv5_1 = nn.ConvTranspose2d(ndf, self.n_channel, kernel_size = kernel_size, stride = stride, padding = padding, bias = False) # 3 * 128 * 128
-        self.deconv5_2 = nn.ConvTranspose2d(ndf, self.n_channel, kernel_size = kernel_size, stride = stride, padding = padding, bias = False) # 3 * 128 * 128
+        self.deconv5_1 = nn.ConvTranspose2d(ndf, self.n_channel, kernel_size = kernel_size, stride = stride, padding = padding, bias = False) 
+        self.deconv5_2 = nn.ConvTranspose2d(ndf, self.n_channel, kernel_size = kernel_size, stride = stride, padding = padding, bias = False) 
 
     def forward(self, z):
-        h1          = F.relu(self.bn1(self.deconv1(z.view(-1, self.z_dim, 1, 1))))
+        h1          = F.relu(self.bn1(self.linear1(z.view(-1, self.z_dim)).view(-1, 8 * self.ndf, self.init_size, self.init_size)))
         h2          = F.relu(self.bn2(self.deconv2(h1)))
         h3          = F.relu(self.bn3(self.deconv3(h2)))
         h4          = F.relu(self.bn4(self.deconv4(h3)))
@@ -50,59 +105,9 @@ class Decoder(nn.Module):
         noise_level = torch.exp(h5_2)
         return rec_img, noise_level
 
-class Encoder(nn.Module):
-    def __init__(self, n_channel, img_size, z_dim):
-        super(Encoder, self).__init__()
-        self.img_size  = img_size
-        self.n_channel = n_channel
-        self.z_dim     = z_dim
-
-
-        self.model    = vision.models.resnet18()
-        self.model.fc = nn.Linear(512, self.z_dim * 2)
-        
-
-        # ndf          = 64 # number of filters
-        # kernel_size  = 4
-        # stride       = 2
-        # padding      = 1
-
-        # self.conv1   = nn.Conv2d(self.n_channel, ndf, kernel_size = kernel_size, stride = stride, padding= padding, bias = False)
-
-        # self.conv2   = nn.Conv2d(1 * ndf, 2 * ndf, kernel_size = kernel_size, stride = stride, padding= padding, bias = False)
-        # self.bn2     = nn.BatchNorm2d(2 * ndf)
-
-        # self.conv3   = nn.Conv2d(2 * ndf, 4 * ndf, kernel_size = kernel_size, stride = stride, padding= padding, bias = False)
-        # self.bn3     = nn.BatchNorm2d(4 * ndf)
-
-        # self.conv4_1 = nn.Conv2d(4 * ndf, self.z_dim, kernel_size = kernel_size, stride = stride, padding= padding, bias = False)
-        # self.bn4_1   = nn.BatchNorm2d(self.z_dim)
-
-        # self.conv4_2 = nn.Conv2d(4 * ndf, self.z_dim, kernel_size = kernel_size, stride = stride, padding= padding, bias = False)
-        # self.bn4_2   = nn.BatchNorm2d(self.z_dim)
-        # 
-        # self.zloc_linear   = nn.Linear(self.z_dim * 4 * 4, self.z_dim)
-        # self.zscale_linear = nn.Linear(self.z_dim * 4 * 4, self.z_dim)
-
-    def forward(self, x):
-        # h1 = F.leaky_relu(self.conv1(x))
-        # h2 = F.leaky_relu(self.bn2(self.conv2(h1)))
-        # h3 = F.leaky_relu(self.bn3(self.conv3(h2)))
-
-        # h_loc   = self.bn4_1(self.conv4_1(h3))
-        # h_scale = self.bn4_2(self.conv4_2(h3))
-
-        # z_loc   = self.zloc_linear(h_loc.view(  -1, self.z_dim * 4 * 4)).squeeze()
-        # z_scale = self.zscale_linear(h_loc.view(-1, self.z_dim * 4 * 4)).squeeze()
-
-        features = self.model(x)
-        z_loc    = features[:, :self.z_dim].squeeze()
-        z_scale  = features[:, self.z_dim:].squeeze()
-
-        return z_loc, F.softplus(z_scale) + 1e-6
 
 class VAE(nn.Module):
-    def __init__(self, n_channel, img_size = 28, z_dim=50, use_cuda=False, conf = dict()):
+    def __init__(self, n_channel, img_size = 64, z_dim=128, use_cuda=False, conf = dict()):
         super(VAE, self).__init__()
         self.img_size = img_size
         self.use_cuda = use_cuda
@@ -111,8 +116,8 @@ class VAE(nn.Module):
         self.decoder  = Decoder(n_channel, img_size, z_dim)
         if use_cuda:
             self.cuda()
-        self.noise_level = conf.get('noise_level',0.1)
-        self.lr          = conf.get('lr', 1e-4)
+        self.noise_level = conf.get('noise_level',0.01)
+        self.lr          = conf.get('lr', 3e-4)
         self.optim       = Adam({"lr": self.lr})
         self.svi         = pyro.infer.SVI(self.model, self.guide, self.optim, loss = Trace_ELBO())
 
